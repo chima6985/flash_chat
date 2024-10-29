dbeadd9a4cc80af78b32c834b6c0e8e1c268fba9
chima6985 <chimaokeke2006@gmail.com>
2024-10-30 00:34:48 +0100

    hero removed

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -21,12 +21,9 @@ class _LoginScreenState extends State<LoginScreen> {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Hero(
-              tag: 'logo',
-              child: Container(
-                height: 200.0,
-                child: Image.asset('images/logo.png'),
-              ),
+            Container(
+              height: 200.0,
+              child: Image.asset('images/logo.png'),
             ),
             const SizedBox(
               height: 48.0,
@@ -64,7 +61,7 @@ class _LoginScreenState extends State<LoginScreen> {
                 //Do something with the user input.
               },
               decoration: const InputDecoration(
-                hintText: 'Enter your passcode.',
+                hintText: 'Enter your password.',
                 contentPadding:
                     EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
                 border: OutlineInputBorder(
