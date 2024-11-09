ac0a200df1eb8bfe0d3d3df16349387a24f57396
chima6985 <chimaokeke2006@gmail.com>
2024-11-09 23:58:28 +0100

    chat

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -58,7 +58,7 @@ class _WelcomeScreenState extends State<WelcomeScreen>
                   ),
                 ),
                 const Text(
-                  'Flash',
+                  'Flash Chat',
                   style: TextStyle(
                     fontSize: 45.0,
                     fontWeight: FontWeight.w900,
