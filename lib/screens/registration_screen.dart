a17c5f99acb1cef2dbeeb32ebf4ff0a4c449ce00
chima6985 <chimaokeke2006@gmail.com>
2024-11-11 10:52:55 +0100

    todays commit

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -40,7 +40,7 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
                 contentPadding:
                     EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
                 border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                  borderRadius: BorderRadius.all(Radius.circular(30.0)),
                 ),
                 enabledBorder: OutlineInputBorder(
                   borderSide: BorderSide(color: Colors.blueAccent, width: 1.0),
