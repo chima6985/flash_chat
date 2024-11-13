d8e78e68d6600fa434f29a1accd34189c681ebee
chima6985 <chimaokeke2006@gmail.com>
2024-11-13 14:02:00 +0100

    animation continuation

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -2,6 +2,7 @@ import 'package:flash_chat/screens/login_screen.dart';
 import 'package:flash_chat/screens/registration_screen.dart';
 import 'package:flutter/material.dart';
 import 'dart:developer';
+import 'package:animated_text_kit/animated_text_kit.dart';
 
 class WelcomeScreen extends StatefulWidget {
   const WelcomeScreen({super.key});
@@ -57,9 +58,9 @@ class _WelcomeScreenState extends State<WelcomeScreen>
                     child: Image.asset('images/logo.png'),  
                   ),
                 ),
-                const Text(
-                  'Flash Chat',
-                  style: TextStyle(
+                  WavyAnimatedTextKit(
+               text: ['Flash Chat'],
+                  textStyle: TextStyle(
                     fontSize: 45.0,
                     fontWeight: FontWeight.w900,
                   ),
